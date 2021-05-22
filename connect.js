4c79ec0fada5ecf81e9d6d8ea8becbb36d58509e
Keita Ito <hlfcrstl@gmail.com>
2021-05-23 00:32:11 +0900

    記録ページの変更

---
+++ connect.js
@@ -0,0 +1,67 @@
+function commit(preDate, record) {
+  let date = new Date(preDate);
+  let row = (date.getTime() - new Date(requestUser().start).getTime()) / 24 / 60 / 60 / 1000 + 2;
+  let column = requestUser().user.number + 1;
+  let sheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName("DATA");
+  if (sheet.getLastRow() < row) {
+    throw "まだ入力できない日付です";
+  }
+  sheet.getRange(row, column).setValue(record);
+}
+
+/**
+ *
+ */
+function requestUser() {
+  let cache = CacheService.getUserCache();
+  let current = cache.get("user");
+  let result = {
+    user: {},
+  };
+  // current = null;
+  if (current) {
+    result = JSON.parse(current);
+  } else {
+    let values = SpreadsheetApp.getActiveSpreadsheet().getSheetByName("USERS").getDataRange().getValues();
+    let header = values.splice(0, 1)[0];
+    for (let i = 0; i < values.length; i++) {
+      if (values[i][0] == Session.getActiveUser().getEmail()) {
+        for (let j = 0; j < values[i].length; j++) {
+          result.user[header[j]] = values[i][j];
+        }
+      }
+    }
+    //
+    result.start = SpreadsheetApp.getActiveSpreadsheet().getSheetByName("DATA").getRange(2, 1).getValue().toString();
+    cache.put("user", JSON.stringify(result));
+  }
+  return result;
+}
+
+function getOverview() {
+  let values = SpreadsheetApp.getActiveSpreadsheet().getSheetByName("DATA").getDataRange().getDisplayValues();
+  let column = requestUser().user.number;
+  let empties = [];
+  let temp = {};
+  for (let i = 1; i < values.length; i++) {
+    let cell = values[i][column];
+    if (!cell) {
+      empties.push(values[i][0]);
+    }
+    temp[cell] = (temp[cell] || 0) + 1;
+  }
+  let mode = [];
+  for (let key of Object.keys(temp)) {
+    mode.push([key, temp[key]]);
+  }
+  mode.sort(function (a, b) {
+    return b[1] - a[1];
+  });
+  mode = mode.map(function (value) {
+    return value[0];
+  });
+  return {
+    empties: empties,
+    mode: mode,
+  };
+}
--- main.js
+++ main.js
@@ -1,3 +1,26 @@
+/**
+ *
+ * @param {object} e
+ * @returns {GoogleAppsScript.HTML.HtmlOutput}
+ */
 function doGet(e) {
   return HtmlService.createTemplateFromFile("index").evaluate().addMetaTag("viewport", "width=device-width, initial-scale=1, shrink-to-fit=no");
+}
+
+/**
+ *
+ */
+function load() {
+  function getLast(sheet) {
+    return sheet.getDataRange().getValues()[sheet.getLastRow() - 1][0];
+  }
+  let dataSheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName("DATA");
+  let last = getLast(dataSheet);
+  let required = new Date();
+  required.setDate(required.getDate() - 2);
+  while (last.getTime() < required.getTime()) {
+    last.setDate(last.getDate() + 1);
+    dataSheet.appendRow([last]);
+    last = getLast(dataSheet);
+  }
 }
